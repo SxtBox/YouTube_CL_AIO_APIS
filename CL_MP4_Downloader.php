56e05e0909160545cba46264e724e851e9c845e1
Albdroid.AL <sxt@usa.com>
2020-12-25 21:52:26 +0100

    Add files via upload

---
+++ CL_MP4_Downloader.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+ ┌─────────────────────────────────────────────────────────────┐
+ |  For More Modules Or Updates Stay Connected to Kodi dot AL  |
+ └─────────────────────────────────────────────────────────────┘
+ ┌───────────┬─────────────────────────────────────────────────┐
+ │ Product   │ Youtube MP4 Stream Extractor                    │
+ │ Version   │ v2.2.1-DEV Multi                                │
+ │ Provider  │ https://www.youtube.com                         │
+ │ Support   │ M3U8/VLC/KODI/SMART TV/XTream Codes/Web Players │
+ │ Licence   │ MIT                                             │
+ │ Author    │ Olsion Bakiaj                                   │
+ │ Email     │ TRC4@USA.COM                                    │
+ │ Author    │ Endrit Pano                                     │
+ │ Email     │ INFO@ALBDROID.AL                                │
+ │ Website   │ https://kodi.al                                 │
+ │ Facebook  │ /albdroid.official/                             │
+ │ Github    │ github.com/SxtBox/                              │
+ │ Created   │ 21 December 2020                                │
+ │ Modified  │ 00:0000:0000                                    │
+ └─────────────────────────────────────────────────────────────┘
+*/
+
+// NOTE: THIS API CONTAIN THIRD PARTY FUNCTIONS
+// HOSTED APIS https://paidcodes.albdroid.al/Youtube_APIS/Downloader/
+// JSON API https://paidcodes.albdroid.al/Youtube_APIS/Downloader/get_stream.php
+
+error_reporting(0);
+set_time_limit(0);
+date_default_timezone_set("Europe/Tirane");
+
+/*
+ Generator @ Kodi dot AL Dev Tools
+ Code For PHP 5/7
+*/
+
+$API_HOST = "https://paidcodes.albdroid.al/Youtube_APIS/Downloader/";
+header("Location: $API_HOST");
+?>
